67512479ac5f96b7f507bf2ce70986f8f4a01c4c
joltzz <guijsilva02@gmail.com>
2022-03-24 13:59:47 -0300

    ajustes finais no movie selection

--- src/componentes/Movie/index.js
+++ src/componentes/Movie/index.js
@@ -4,44 +4,43 @@ import axios from "axios";
 
 import Loading from "../../assets/img/loading.svg";
 
-import "./Movie.css"
+import "../Movie/Movie.css"
 
 function MovieSelection() {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies")
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
         promise.then(response => {
-            setMovies(response);
-        })
-    }, []);
+            setMovies(response.data);
+        });
+    }, [])
 
     if (movies.length === 0) {
         return (
             <div className="loading">
-                <img src={Loading} />
+                <img src={Loading} alt=""/>
                 <h1>Carregando...</h1>
             </div>
-        )
+        );
     }
 
     return (
-        <div className="Movies">
-            <main className="movie-selection-page">
-                <div className="title-page">
-                    <span className="title">Selecione o filme</span>
-                </div>
-                <div className="content">
-                    {movies.map(movie => (
-                        <Link to={`/sessions/${movie.id}`}>
-                            <div className="container">
-                                <img src={movie.posterURL} alt={movie.title} />
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </main>
-        </div>
+        <main className="movie-selection-page">
+            <div className="title-page">
+                <span className="title">Selecione o filme</span>
+            </div>
+            <div className="content">
+                {movies.map(movie => (
+                    <Link to={`/sessions/${movie.id}`}>
+                        <div className="container">
+                            <img src={movie.posterURL} alt={movie.title} />
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </main>
     )
 }
 
